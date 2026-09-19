6dc70619058effb41e9cd9dbdaefca2a7e0b1d47
CyberSurgeon01 <rsrahulchandra@gmail.com>
2026-09-19 22:13:07 +0600

    Implement Oracle connection in config.php

--- config.php
+++ config.php
@@ -1,4 +1,9 @@
 <?php
 // config.php
-// Empty for now, will hold Oracle connection later
+$conn = @oci_connect('username', 'password', 'localhost/XE');
+if (!$conn) {
+    $e = oci_error();
+    // Intentionally not terminating script here to allow the UI logic to output the error string nicely
+    // die("Database Connection Failed: " . htmlentities($e['message'], ENT_QUOTES));
+}
 ?>
