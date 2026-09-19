a60f37910c53ce5a210821266a200c3282a6b313
CyberSurgeon01 <rsrahulchandra@gmail.com>
2026-09-19 21:59:58 +0600

    Update navbar to include logout link and display active role

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,5 +1,13 @@
+<?php
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+?>
 <nav>
     <ul>
-        <li><a href="../index.php">Home</a></li>
+        <li><a href="../index.php">Home / Dashboard</a></li>
+        <?php if (isset($_SESSION['logged_in']) && $_SESSION['logged_in']): ?>
+            <li><a href="../index.php?action=logout" style="color: #ffb3b3;">Logout (<?= htmlspecialchars($_SESSION['role']) ?>)</a></li>
+        <?php endif; ?>
     </ul>
 </nav>
